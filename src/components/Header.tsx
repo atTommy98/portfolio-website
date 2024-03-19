f6d696452c5ed72d86273d030a835bb816e4ccc2
Tommy <tommy_holt@icloud.com>
2024-03-19 14:51:11 +0000

    Animation of name in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,18 +17,28 @@ export default function Header() {
   ];
 
   return (
-    <header className="h-[10vh] w-full flex items-center justify-between px-12">
-      <div className="flex justify-center items-center">
-        <h1 className="dark-text dark:light-text">Tommy Holt</h1>
+    <header className="h-[7.5vh] min-h-fit w-full flex items-center justify-between px-2 md:px-8 bg-sky-400 dark:bg-sky-700 shadow-md text-sm md:text-xl">
+      <div className="relative flex justify-center items-center overflow-hidden">
+        <h1 className="dark-text dark:light-text uppercase text-center font-bold absolute animate-swap1">
+          Developer
+        </h1>
+        <h1 className="dark-text dark:light-text uppercase text-center font-bold relative animate-swap2">
+          Tommy Holt
+        </h1>
+        <h1 className="dark-text dark:light-text uppercase text-center font-bold absolute animate-swap3">
+          Adventurer
+        </h1>
       </div>
-      <div className="flex justify-center items-center">
+      <nav className="flex justify-center items-center">
         {links.map((link) => (
-          <a key={link.id} className="dark-text dark:light-text mx-4">
+          <a
+            key={link.id}
+            className="dark-text dark:light-text mx-2 md:mx-4 w-fit">
             {link.name}
           </a>
         ))}
         <ThemeButton />
-      </div>
+      </nav>
     </header>
   );
 }
--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,10 +1,6 @@
-/* typescript-eslint-disable */
-
 import { useState } from "react";
 import { useTheme } from "next-themes";
 import useSound from "use-sound";
-import CLICK1 from "../assets/sounds/CLICK1.mp3";
-import CLICK2 from "../assets/sounds/CLICK2.mp3";
 import Image from "next/image";
 import SUN from "../assets/SUN.svg";
 import MOON from "../assets/MOON.svg";
@@ -14,27 +10,29 @@ export default function ThemeButton() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
 
+  const [sound1] = useSound("/sounds/CLICK1.mp3", { volume: 0.25 });
+  const [sound2] = useSound("/sounds/CLICK2.mp3", { volume: 0.25 });
+
   function handleThemeToggle() {
     setAnimate(true);
     if (theme === "dark") {
-        setTheme("light")
-        playClicked1()
+      setTheme("light");
+      sound1();
     }
     if (theme === "light") {
-        setTheme("dark")
-        playClicked2()
+      setTheme("dark");
+      sound2();
     }
     theme === "dark" ? setTheme("light") : setTheme("dark");
   }
 
-  const [playClicked1] = useSound("/sounds/CLICK1.mp3", { volume: 0.25 });
-  const [playClicked2] = useSound("/sounds/CLICK2.mp3", { volume: 0.25 });
-
   return (
-    <div className="relative max-w-[30px] m-4">
+    <div className="relative max-w-[25px] md:max-w-[35px] m-[2px] sm:m-2 md:m-4 cursor-pointer">
       <Image
         alt="Moon or sun icon based on currently set theme"
-        className={`w-full dark:invert ${animate && "animate-wiggle"} opacity-80`}
+        className={`w-full dark:invert ${
+          animate && "animate-wiggle"
+        } opacity-80`}
         src={theme === "dark" ? MOON : SUN}
         onClick={handleThemeToggle}
         onAnimationEnd={() => setAnimate(false)}
